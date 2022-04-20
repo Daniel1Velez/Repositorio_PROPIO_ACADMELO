8591897084a2bd4e383e8978e08a84659f0de531
Daniel Velez <danielvelez994@hotmail.com>
2022-04-20 04:37:52 -0500

    Nuevo diseño

---
+++ Portafolio/Tienda_Online/data/db.js
@@ -0,0 +1,26 @@
+export const items = [
+    {
+      id: 1,
+      name: 'Bumpers',
+      price: 1400.00,
+      image: 'assets/Images/Defensa.jpg',
+      category: 'bumpers',
+      quantity: 8
+    },
+    {
+      id: 2,
+      name: 'Side Skirts',
+      price: 800.00,
+      image: 'assets/Images/Escalon.jpg',
+      category: 'side skirts',
+      quantity: 10
+    },
+    {
+      id: 3,
+      name: 'Roll Cage',
+      price: 2400.00,
+      image: 'assets/Images/Barra Antivuelco.jpg',
+      category: 'roll cage',
+      quantity: 6
+    }
+  ]
